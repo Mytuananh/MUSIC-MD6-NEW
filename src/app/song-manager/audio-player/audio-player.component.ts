22acd1e2ca849964ca567b29eb721ef7b5460afa
Your Name <you@example.com>
2022-01-19 09:30:14 +0700

    done count

--- src/app/song-manager/audio-player/audio-player.component.ts
+++ src/app/song-manager/audio-player/audio-player.component.ts
@@ -1,137 +1,331 @@
-import {AfterViewInit, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {Observable} from 'rxjs';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild
+} from '@angular/core';
+import {Observable, Subject} from 'rxjs';
 import {SongService} from '../../service/song.service';
 import * as moment from "moment";
+import {Song} from '../../model/Song';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatTableDataSource} from '@angular/material/table';
 
 @Component({
   selector: 'app-audio-player',
   templateUrl: './audio-player.component.html',
   styleUrls: ['./audio-player.component.scss']
 })
-export class AudioPlayerComponent implements OnInit,AfterViewInit {
-  @Input() songs: any;
+export class AudioPlayerComponent implements OnInit,OnChanges {
+  @Input()
+  set playlist(playlist: Song[]) {
+    this.songService.setPlaylist(playlist);
+  }
   @Output() eventEmitter = new EventEmitter();
 
-  constructor(private songService: SongService) {
+  constructor(private songService: SongService,
+              elem: ElementRef) {
+
   }
 
-  ngAfterViewInit(): void {
-    this.songService.checkCurrent$.subscribe(data => {
-      this.currentIndex = Number(data);
-      this.play();
-    })
+  @ViewChild(MatPaginator, { static: false }) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.setDataSourceAttributes();
   }
 
-  ngOnInit(): void {
-    this.currentIndex = 0;
-    this.currentTime = 0;
-    this.totalTime = 0;
-    this.checkplay = true;
-    this.currentTime = moment.utc(0).format("mm:ss");
-    this.totalTime = moment.utc(0).format("mm:ss");
-  }
+  displayedColumns: string[];
+  dataSource = new MatTableDataSource<Song>();
+  paginator: MatPaginator;
+  repeat = 'all';
 
-  audio = new Audio();
-  audioEvents = [
-    "ended",
-    "error",
-    "play",
-    "playing",
-    "pause",
-    "timeupdate",
-    "canplay",
-    "loadedmetadata",
-    "loadstart"
-  ];
+
+  songs: Song[] = [];
+
+  @Input() displayName = true;
+  @Input() displayPlaylist = true;
+  @Input() displayVolumeControls = true;
+  @Input() displayRepeatControls = true;
+  @Input() pageSizeOptions = [10, 20, 30];
+  @Input() expanded = true;
+  @Input() autoPlay = false;
+  @Input() disablePositionSlider = false;
+  @Input() displaySinger = false;
+  @Input() displayDuration = false;
+
+  // Support for internationalization
+  @Input() tableHeader = 'Playlist';
+  @Input() nameHeader = 'name';
+  @Input() singerHeader = 'singer';
+  @Input() durationHeader = 'Duration';
+
+
   currentIndex = 0;
-  currentTime: any;
-  currentRange = 0;
-  totalRang = 0;
-  totalTime: any;
-  checkplay = true;
-  nameSong: any;
 
-  play() {
-    this.eventEmitter.emit(this.currentIndex);
-    this.nameSong = this.songs[this.currentIndex].name;
-    if (this.checkplay && this.currentRange == 0) {
-      this.streamObserver(this.songs[this.currentIndex].file).subscribe();
-      this.checkplay = false;
-    } else if (this.checkplay && this.currentRange != 0) {
-      this.audio.play();
-      this.checkplay = false;
+  @Output()
+  songEnded: Subject<string> = new Subject<string>();
+
+  @ViewChild('audioPlayer', { static: true }) player: ElementRef;
+
+  iOS = (/iPad|iPhone|iPod/.test(navigator.platform)
+    || (navigator.platform === 'MacIntel' && navigator.maxTouchPoints > 1));
+
+  loaderDisplay = false;
+  isPlaying = false;
+  currentTime = 0;
+  volume = 0.1;
+  duration = 0.01;
+
+  private startOffsetValue = 0;
+  @Input()
+  set startOffset(seconds: number) {
+    this.startOffsetValue = seconds;
+    this.player.nativeElement.currentTime = seconds;
+  }
+  get startOffset(): number {
+    return this.startOffsetValue;
+  }
+
+  @Input()
+  public endOffset = 0;
+
+  currTimePosChanged(event) {
+    this.player.nativeElement.currentTime = event.value;
+  }
+
+  bindPlayerEvent(): void {
+
+    this.player.nativeElement.addEventListener('playing', () => {
+      this.isPlaying = true;
+      this.duration = Math.floor(this.player.nativeElement.duration);
+    });
+    this.player.nativeElement.addEventListener('pause', () => {
+      this.isPlaying = false;
+    });
+    this.player.nativeElement.addEventListener('timeupdate', () => {
+      this.currentTime = Math.floor(this.player.nativeElement.currentTime);
+    });
+    this.player.nativeElement.addEventListener('volume', () => {
+      this.volume = Math.floor(this.player.nativeElement.volume);
+    });
+    if (!this.iOS) {
+      this.player.nativeElement.addEventListener('loadstart', () => {
+        this.loaderDisplay = true;
+      });
+    }
+    this.player.nativeElement.addEventListener('loadedmetadata', () => {
+      this.loaderDisplay = false;
+      this.duration = Math.floor(this.player.nativeElement.duration);
+    });
+    this.player.nativeElement.addEventListener('ended', () => {
+      this.songEnded.next('ended');
+    });
+
+  }
+
+  playBtnHandler(): void {
+    if (this.loaderDisplay) {
+      return;
+    }
+    if (this.player.nativeElement.paused) {
+      if (this.currentTime >= this.duration - this.endOffset) {
+        this.player.nativeElement.currentTime = this.startOffset;
+      } else {
+        this.player.nativeElement.currentTime = this.currentTime;
+      }
+
+      this.player.nativeElement.play();
     } else {
-      this.audio.pause()
-      this.checkplay = true;
+      this.currentTime = this.player.nativeElement.currentTime;
+      this.player.nativeElement.pause();
     }
   }
 
-  next() {
-    this.currentRange = 0;
-    this.checkplay = true;
-    if (this.currentIndex < this.songs.length) {
-      this.currentIndex++;
-    } else {
-      this.currentIndex = 0;
+  play(song?: Song): void {
+    if (song) {
+      this.startOffset = song.startOffset || 0;
+      this.endOffset = song.endOffset || 0;
     }
-    this.play();
+
+    setTimeout(() => {
+      this.player.nativeElement.play();
+    }, 50);
+
   }
 
-  prev() {
-    this.currentRange = 0;
-    this.checkplay = true;
-    if (this.currentIndex == 0) {
-      this.currentIndex = this.songs.length;
+  toggleVolume() {
+    if (this.volume === 0) {
+      this.setVolume(1.0);
     } else {
-      this.currentIndex--;
+      this.setVolume(0);
     }
-    this.play();
   }
 
+  toggleRepeat() {
+    if (this.repeat === 'none') {
+      this.repeat = 'all';
+    } else if (this.repeat === 'all') {
+      if (this.songs.length > 1) {
+        this.repeat = 'one';
+      } else {
+        this.repeat = 'none';
+      }
+    } else if (this.repeat === 'one' && this.songs.length > 1) {
+      this.repeat = 'none';
+    }
+  }
 
-  streamObserver(url: string) {
-    return new Observable(() => {
-      this.audio.src = url;
-      this.audio.load();
-      this.audio.play();
-      const handler = (event: Event) => {
-        this.currentTime = moment.utc(this.audio.currentTime * 1000).format("mm:ss");
-        this.totalTime = moment.utc(this.audio.duration * 1000).format("mm:ss");
-        this.totalRang = this.audio.duration;
-        this.currentRange = this.audio.currentTime;
-        if (this.audio.currentTime == this.audio.duration) {
-          this.next();
+  private setVolume(vol) {
+    this.volume = vol;
+    this.player.nativeElement.volume = this.volume;
+  }
+
+  ngOnInit() {
+
+    this.bindPlayerEvent();
+
+    // auto play next track
+    this.player.nativeElement.addEventListener('ended', () => {
+      if (this.checkIfSongHasStartedSinceAtleastTwoSeconds()) {
+        if (this.repeat === 'all') {
+          this.nextSong();
+        } else if (this.repeat === 'one') {
+          this.play();
+        } else if (this.repeat === 'none') {
+          // Do nothing
         }
       }
-      this.addEvent(this.audio, this.audioEvents, handler);
-      return () => {
-        this.audio.pause();
-        this.audio.currentTime = 0;
-        this.removeEvent(this.audio, this.audioEvents, handler)
+    });
+
+    this.player.nativeElement.addEventListener('timeupdate', () => {
+      this.songService.setCurrentTime(this.player.nativeElement.currentTime);
+    });
+
+    // Subscribe to playlist observer from AudioPlayerService and
+    // update the playlist within MatAdvancedAudioPlayerComponent
+    this.songService.getPlaylist().subscribe(songs => {
+      if (songs !== null && songs !== []) {
+        this.songs = songs;
+        this.initialize();
       }
-    })
+    });
+
   }
 
-  private addEvent(audio: HTMLAudioElement, audioEvents: string[], handler: (event: Event) => void) {
-    audioEvents.forEach(event => {
-      audio.addEventListener(event, handler)
-    })
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.hasOwnProperty('displaySinger') || changes.hasOwnProperty('displayDuration')) {
+      this.buildDisplayedColumns();
+    }
   }
 
-  private removeEvent(audio: HTMLAudioElement, audioEvents: string[], handler: (event: Event) => void) {
-    audioEvents.forEach(event => {
-      audio.removeEventListener(event, handler)
-    })
+  private buildDisplayedColumns() {
+    this.displayedColumns = ['name'];
+    if (this.displaySinger) {
+      this.displayedColumns.push('singer');
+    }
+    if (this.displayDuration) {
+      this.displayedColumns.push('duration');
+    }
+    this.displayedColumns.push('status');
   }
 
-  setProsess(myRange: HTMLInputElement) {
-    this.audio.currentTime = Number(myRange.value);
+  initialize() {
+    this.buildDisplayedColumns();
+
+    // populate indexs for the track and configure
+    // material table data source and paginator
+    this.setDataSourceAttributes();
+
+
+    this.player.nativeElement.currentTime = this.startOffset;
+    this.updateCurrentSong();
+
+    if (this.autoPlay) {
+      this.play();
+    }
   }
 
-  ngOnDestroy(): void {
-    this.audio.pause();
-    this.audio.currentTime = 0;
-    this.checkplay = true;
+  setDataSourceAttributes() {
+    let index = 1;
+    if (this.songs) {
+      this.songs.forEach((song: Song) => {
+        song.index = index++;
+      });
+      this.dataSource = new MatTableDataSource<Song>(this.songs);
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  nextSong(): void {
+    if (this.displayPlaylist === true
+      && (((this.currentIndex + 1) % this.paginator.pageSize) === 0
+        || (this.currentIndex + 1) === this.paginator.length)) {
+      if (this.paginator.hasNextPage()) {
+        this.paginator.nextPage();
+      } else if (!this.paginator.hasNextPage()) {
+        this.paginator.firstPage();
+      }
+    }
+    this.currentTime = 0;
+    this.duration = 0.01;
+    if ((this.currentIndex + 1) >= this.songs.length) {
+      this.currentIndex = 0;
+    } else {
+      this.currentIndex++;
+    }
+    this.updateCurrentSong();
+    this.play();
+  }
+
+  previousSong(): void {
+    this.currentTime = 0;
+    this.duration = 0.01;
+    if (!this.checkIfSongHasStartedSinceAtleastTwoSeconds()) {
+      if (this.displayPlaylist === true
+        && (((this.currentIndex) % this.paginator.pageSize) === 0
+          || (this.currentIndex === 0))) {
+        if (this.paginator.hasPreviousPage()) {
+          this.paginator.previousPage();
+        } else if (!this.paginator.hasPreviousPage()) {
+          this.paginator.lastPage();
+        }
+      }
+      if ((this.currentIndex - 1) < 0) {
+        this.currentIndex = (this.songs.length - 1);
+      } else {
+        this.currentIndex--;
+      }
+    } else {
+      this.resetSong();
+    }
+    this.updateCurrentSong();
+    this.play();
+  }
+
+  resetSong(): void {
+    this.player.nativeElement.src = this.songs[this.currentIndex].file;
+  }
+  selectSong(index: number): void {
+    this.currentIndex = index - 1;
+    this.updateCurrentSong();
+    this.play();
+  }
+
+  checkIfSongHasStartedSinceAtleastTwoSeconds(): boolean {
+    return this.player.nativeElement.currentTime > 2;
+  }
+
+  updateCurrentSong() {
+    this.songService.setCurrentSong(this.songs[this.currentIndex]);
+    if (this.checkIfSongHasStartedSinceAtleastTwoSeconds()){
+      this.songService.updateCount(this.songs[this.currentIndex].id).subscribe(()=>{});
+    }
+
   }
 
 }
